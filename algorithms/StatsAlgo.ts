6c87bde9068ac62816a238cb51157ac50d94a1e6
Augustin MORICEAU <augustin.moriceau@orange.fr>
2025-10-15 08:29:45 +0100

    feat : get minimalBoxes

--- algorithms/StatsAlgo.ts
+++ algorithms/StatsAlgo.ts
@@ -1,1 +1,25 @@
-import warehouse from "../utils/WarehouseUtils";
+import { Warehouse } from "../models/warehouse";
+import { Order } from "../models/order";
+import { BoxCapacity } from "../models/box";
+
+export function getTotalWeight(warehouse: Warehouse): number {
+    let totalWeight = 0;
+    for (const order of warehouse.orders as Order[]) {
+        totalWeight += Array.from(order.products.entries()).reduce((sum, [product, quantity]) => sum + product.weight * quantity, 0);
+    }
+    return totalWeight;
+}
+
+export function getTotalVolume(warehouse: Warehouse): number {
+    let totalVolume = 0;
+    for (const order of warehouse.orders as Order[]) {
+        totalVolume += Array.from(order.products.entries()).reduce((sum, [product, quantity]) => sum + product.volume * quantity, 0);
+    }
+    return totalVolume;
+}  
+
+export function minimalNumberOfBoxes(warehouse: Warehouse): number {
+    let minimalBoxForWeight = Math.ceil(getTotalWeight(warehouse) / BoxCapacity.WEIGTH);
+    let minimalBoxForVolume = Math.ceil(getTotalVolume(warehouse) / BoxCapacity.VOLUME);
+    return Math.max(minimalBoxForWeight, minimalBoxForVolume);
+}
