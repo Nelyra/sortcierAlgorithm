0840685f5177524893f79831af70003b33088b7c
Namu <49788407+namuuu@users.noreply.github.com>
2025-10-24 09:06:53 +0200

    feat: output everything

--- utils/PrintUtils.ts
+++ utils/PrintUtils.ts
@@ -2,24 +2,45 @@ import warehouse from "./WarehouseUtils";
 import * as stats from "../algorithms/StatsAlgo";
 import { binPackingV3 } from "../algorithms/BinPackingV3"
 import { binPackingV4 } from "../algorithms/BinPackingV4"
-import { BoxCapacity } from "../models/box";
+import { Box, BoxCapacity } from "../models/box";
 import { setupAlleys } from "./AlleyUtils";
+import { Trolley } from "../models/trolley";
+import { Product } from "../models/product";
 
 export function PrintTest() {
     console.log("Test Print");
+
+    const trolley: Trolley = {
+        id: 0,
+        boxes: []
+    }
+
     // for (const alley of warehouse.alleys || []) {
     //     console.log(`Alley ${alley.name} (ID: ${alley.id}): Locations: ${alley.locationIds.join(", ")}`);
     // }
     const solution = binPackingV4(warehouse.orders[0], BoxCapacity.WEIGTH, BoxCapacity.VOLUME);
     for (const colis of solution) {
+        const box: Box = {
+            id: trolley.boxes.length,
+            maxVolume: BoxCapacity.VOLUME,
+            maxWeight: BoxCapacity.WEIGTH,
+            products: new Map<Product, number>()
+        }
+
         for (const product of colis) {
             console.log(`Product ID: ${product.idx}, Weight: ${product.weight}, Volume: ${product.volume}, Alley: ${warehouse.alleys?.find(alley => alley.locationIds.includes(product.location))?.id || "Unknown"}`);
+            box.products.set(product, (box.products.get(product) || 0) + 1);
         }
+
+        trolley.boxes.push(box);
 
         console.log("Total weight:", colis.reduce((sum, product) => sum + product.weight, 0));
         console.log("Total volume:", colis.reduce((sum, product) => sum + product.volume, 0));
         console.log(" ")
     }
+
+    warehouse.trolleys.push(trolley);
+    console.log("Trolley:", JSON.stringify(trolley, null, 2));
 }
 
 export function PrintWarehouse() {
